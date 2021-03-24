b084be67d8a1a39f527f9fa50cfe629d38b4a31e
yRodrigo2219 <rodrigo2219dg@gmail.com>
2021-03-24 09:58:43 -0300

    Structures Events + Main Action Logic(Events Red.)

--- src/store/ducks/events/index.js
+++ src/store/ducks/events/index.js
@@ -1,9 +1,38 @@
 import { GAME } from '../gameInfo/types';
+import { EVENTS, EVENT } from './types';
 
 const INITIAL_STATE = {
-  pastFrame: {},
+  pastFrame: null,
   events: [],
 };
+
+function getStructuresDestroyed(events, team, pastTeam, side) {
+  const pastTowers = pastTeam.towers;
+  const towers = team.towers;
+  // One Tower Destroyed Event is triggered for each tower destroyed
+  // in the meantime
+  for (let i = 0; i < (towers - pastTowers); i++)
+    events.push({
+      type: EVENTS.STRUCTURE,
+      data: {
+        side: side,
+        structure: EVENT.STRUCTURE.TOWER
+      }
+    });
+
+  const pastInhibs = pastTeam.inhibitors;
+  const inhibs = team.inhibitors;
+  // One Inhibitor Destroyed Event is triggered for each inhibitor destroyed
+  // in the meantime
+  for (let i = 0; i < (inhibs - pastInhibs); i++)
+    events.push({
+      type: EVENTS.STRUCTURE,
+      data: {
+        side: side,
+        structure: EVENT.STRUCTURE.INHIB
+      }
+    });
+}
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
@@ -13,10 +42,25 @@ export default function reducer(state = INITIAL_STATE, action) {
       // TODO: Check if its updating the correct game
       const metaData = action.payload.gameMetadata;
       const dataFrames = action.payload.frames;
-      const events = [];
 
       const lastFrame = dataFrames[dataFrames.length - 1];
+      const events = [];
 
+      if (state.pastFrame !== null) {
+        let pastFrame = state.pastFrame;
+        dataFrames.forEach(frame => {
+
+          // TODOS:
+          // Kill Event
+          getStructuresDestroyed(events, frame.blueTeam, pastFrame.blueTeam, 'blue');
+          getStructuresDestroyed(events, frame.redTeam, pastFrame.redTeam, 'red');
+          // Dragon Events
+          // Epic Monster Events
+          // Game Status Events
+
+          pastFrame = frame;
+        });
+      }
 
       return {
         ...state,
