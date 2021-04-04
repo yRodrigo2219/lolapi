71b7ec4935263e816858304c00dc1942f7d79873
yRodrigo2219 <rodrigo2219dg@gmail.com>
2021-04-04 13:27:55 -0300

    Router basename fix

--- src/Routes.js
+++ src/Routes.js
@@ -11,7 +11,7 @@ import Match from './pages/Match';
 
 export default function Routes() {
   return (
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
       <Header />
 
       <Switch>
